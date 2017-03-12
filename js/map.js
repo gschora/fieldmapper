e70e08d55d28404cb564715fd4f8831f0cc851b4
hannes <hannes.dummy@putz-transporte.at>
2017-03-12 23:34:46 +0100

    measure area and lines work deleting feature with button works

--- js/map.js
+++ js/map.js
@@ -1,12 +1,9 @@
 var capabilitiesUrl = 'https://www.basemap.at/wmts/1.0.0/WMTSCapabilities.xml';
 
-// HiDPI support:
-// * Use 'bmaphidpi' layer (pixel ratio 2) for device pixel ratio > 1
-// * Use 'geolandbasemap' layer (pixel ratio 1) for device pixel ratio == 1
 var hiDPI = ol.has.DEVICE_PIXEL_RATIO > 1;
 
 var features = new ol.Collection();
-var source = new ol.source.Vector({ features: features });
+var source = new ol.source.Vector({ feature: features });
 var vector = new ol.layer.Vector({
     source: source,
     style: new ol.style.Style({
@@ -30,17 +27,7 @@ var tilePixelRatio = hiDPI ? 2 : 1;
 
 var wgs84Sphere = new ol.Sphere(6378137);
 
-//var mousePositionControl = new ol.control.MousePosition({
-//    coordinateFormat: ol.coordinate.createStringXY(0),
-//    projection: 'EPSG:3857',
-//    // comment the following two lines to have the mouse position
-//    // be placed within the map.
-//    // className: 'custom-mouse-position',
-//    // target: document.getElementById('mouse-position'), 
-//    undefinedHTML: '&nbsp;'
-//});
-
-var measureMode = "LineString";
+var measureMode = "none";
 /**
        * @constructor
        * @extends {ol.control.Control}
@@ -55,9 +42,16 @@ var MeasureLineControl = function (opt_options) {
 
     var this_ = this;
     var setMeasureLine = function () {
-        measureMode = "LineString";
-        map.removeInteraction(draw);
-        addInteraction();
+        if (measureMode === "LineString") {
+            measureMode = "none";
+            map.removeInteraction(draw);
+            map.addInteraction(select);
+            map.addInteraction(modify);
+        } else {
+            measureMode = "LineString";
+            map.removeInteraction(draw);
+            addInteraction();
+        }
     };
 
     button.addEventListener('click', setMeasureLine, false);
@@ -89,9 +83,16 @@ var MeasureAreaControl = function (opt_options) {
 
     var this_ = this;
     var setMeasureArea = function () {
-        measureMode = "Polygon";
-        map.removeInteraction(draw);
-        addInteraction();
+        if (measureMode === "Polygon") {
+            measureMode = "none";
+            map.removeInteraction(draw);
+            map.addInteraction(select);
+            map.addInteraction(modify);
+        } else {
+            measureMode = "Polygon";
+            map.removeInteraction(draw);
+            addInteraction();
+        }
     };
 
     button.addEventListener('click', setMeasureArea, false);
@@ -109,7 +110,32 @@ var MeasureAreaControl = function (opt_options) {
 };
 ol.inherits(MeasureAreaControl, ol.control.Control);
 
-//var map_stdZoom = 
+var el_btn_feature_del;
+/**
+       * @constructor
+       * @extends {ol.control.Control}
+       * @param {Object=} opt_options Control options.
+       */
+var FeatureDeleteControl = function (opt_options) {
+
+    var options = opt_options || {};
+
+    var button = document.createElement('button');
+    button.id = 'btn_feature_del';
+    button.innerHTML = 'X';
+
+    el_btn_feature_del = document.createElement('div');
+    el_btn_feature_del.className = 'ol-control btn_feature_del'; //ol-unselectable 
+    el_btn_feature_del.appendChild(button);
+    el_btn_feature_del.hidden = true;
+
+    ol.control.Control.call(this, {
+        element: el_btn_feature_del,
+        target: options.target
+    });
+
+};
+ol.inherits(FeatureDeleteControl, ol.control.Control);
 
 var map = new ol.Map({
     target: 'map',
@@ -119,7 +145,7 @@ var map = new ol.Map({
             collapsible: false
         })
     }).extend([
-          new MeasureLineControl(), new MeasureAreaControl()
+          new MeasureLineControl(), new MeasureAreaControl(), new FeatureDeleteControl()
     ]),
     view: new ol.View({
         center: (function () {
@@ -132,7 +158,7 @@ var map = new ol.Map({
                 return hm;
             }
         })(),
-        zoom: (function() {
+        zoom: (function () {
             if (localStorage.map_stdZoom === undefined) {
                 return 6;
             } else { return parseInt(localStorage.map_stdZoom); }
@@ -157,13 +183,11 @@ fetch(capabilitiesUrl).then(function (response) {
     map.addLayer(vector);
 });
 
-
 /**
-       * Currently drawn feature.
-       * @type {ol.Feature}
-       */
+ * Currently drawn feature.
+ * @type {ol.Feature}
+ */
 var sketch;
-
 
 /**
  * The help tooltip element.
@@ -171,13 +195,11 @@ var sketch;
  */
 var helpTooltipElement;
 
-
 /**
  * Overlay to show the help messages.
  * @type {ol.Overlay}
  */
 var helpTooltip;
-
 
 /**
  * The measure tooltip element.
@@ -185,13 +207,11 @@ var helpTooltip;
  */
 var measureTooltipElement;
 
-
 /**
  * Overlay to show the measurement.
  * @type {ol.Overlay}
  */
 var measureTooltip;
-
 
 /**
  * Message to show when the user is drawing a polygon.
@@ -199,52 +219,13 @@ var measureTooltip;
  */
 var continuePolygonMsg = 'Click to continue drawing the polygon';
 
-
 /**
  * Message to show when the user is drawing a line.
  * @type {string}
  */
 var continueLineMsg = 'Click to continue drawing the line';
 
-
-///**
-// * Handle pointer move.
-// * @param {ol.MapBrowserEvent} evt The event.
-// */
-//var pointerMoveHandler = function (evt) {
-//    if (evt.dragging) {
-//        return;
-//    }
-//    /** @type {string} */
-//    var helpMsg = 'Click to start drawing';
-
-//    if (sketch) {
-//        var geom = (sketch.getGeometry());
-//        if (geom instanceof ol.geom.Polygon) {
-//            helpMsg = continuePolygonMsg;
-//        } else if (geom instanceof ol.geom.LineString) {
-//            helpMsg = continueLineMsg;
-//        }
-//    }
-
-//    helpTooltipElement.innerHTML = helpMsg;
-//    helpTooltip.setPosition(evt.coordinate);
-
-//    helpTooltipElement.classList.remove('hidden');
-//};
-
-//map.on('pointermove', pointerMoveHandler);
-
-//map.getViewport().addEventListener('mouseout', function () {
-//    helpTooltipElement.classList.add('hidden');
-//});
-
-//var typeSelect = document.getElementById('type');
-//var geodesicCheckbox = document.getElementById('geodesic');
-
 var draw; // global so we can remove it later
-
-
 /**
  * Format length output.
  * @param {ol.geom.LineString} line The line.
@@ -252,7 +233,6 @@ var draw; // global so we can remove it later
  */
 var formatLength = function (line) {
     var length;
-    //if (geodesicCheckbox.checked) {
     var coordinates = line.getCoordinates();
     length = 0;
     var sourceProj = map.getView().getProjection();
@@ -261,9 +241,7 @@ var formatLength = function (line) {
         var c2 = ol.proj.transform(coordinates[i + 1], sourceProj, 'EPSG:4326');
         length += wgs84Sphere.haversineDistance(c1, c2);
     }
-    //} else {
-    //    length = Math.round(line.getLength() * 100) / 100;
-    //}
+
     var output;
     if (length > 1000) {
         output = (Math.round(length / 1000 * 100) / 100) +
@@ -283,28 +261,24 @@ var formatLength = function (line) {
  */
 var formatArea = function (polygon) {
     var area;
-    //if (geodesicCheckbox.checked) {
     var sourceProj = map.getView().getProjection();
     var geom = /** @type {ol.geom.Polygon} */(polygon.clone().transform(
         sourceProj, 'EPSG:4326'));
     var coordinates = geom.getLinearRing(0).getCoordinates();
     area = Math.abs(wgs84Sphere.geodesicArea(coordinates));
-    //} else {
-    //    area = polygon.getArea();
-    //}
+
     var output;
     if (area > 10000) {
-        output = (Math.round(area / 1000000 * 100) / 100) +
-            ' ' + 'km<sup>2</sup>';
+        output = (Math.round((area/10000)*1000)/1000) +
+            ' ' + 'ha';
     } else {
         output = (Math.round(area * 100) / 100) +
             ' ' + 'm<sup>2</sup>';
     }
     return output;
 };
-
+var fid = 0;
 function addInteraction() {
-    //console.log("measuremode: " + measureMode);
     draw = new ol.interaction.Draw({
         source: source,
         type: /** @type {ol.geom.GeometryType} */ (measureMode),
@@ -328,61 +302,116 @@ function addInteraction() {
             })
         })
     });
+    map.removeInteraction(modify);
+    map.removeInteraction(select);
     map.addInteraction(draw);
+    var tooltipListener;
+    draw.on('drawstart', function (evt) {
+        // set sketch
+        sketch = evt.feature;
 
-    var listener;
-    draw.on('drawstart',
-        function (evt) {
-            // set sketch
-            sketch = evt.feature;
+        /** @type {ol.Coordinate|undefined} */
+        var tooltipCoord = evt.coordinate;
 
-            /** @type {ol.Coordinate|undefined} */
-            var tooltipCoord = evt.coordinate;
+        tooltipListener = sketch.getGeometry().on('change', function (evt) {
+            var geom = evt.target;
+            var output;
+            if (geom instanceof ol.geom.Polygon) {
+                output = formatArea(geom);
+                tooltipCoord = geom.getInteriorPoint().getCoordinates();
+            } else if (geom instanceof ol.geom.LineString) {
+                output = formatLength(geom);
+                tooltipCoord = geom.getLastCoordinate();
+            }
+            measureTooltipElement.innerHTML = output;
+            measureTooltip.setPosition(tooltipCoord);
+        });
+    }, this);
 
-            listener = sketch.getGeometry().on('change', function (evt) {
-                var geom = evt.target;
-                var output;
-                if (geom instanceof ol.geom.Polygon) {
-                    output = formatArea(geom);
-                    tooltipCoord = geom.getInteriorPoint().getCoordinates();
-                } else if (geom instanceof ol.geom.LineString) {
-                    output = formatLength(geom);
-                    tooltipCoord = geom.getLastCoordinate();
-                }
-                measureTooltipElement.innerHTML = output;
-                measureTooltip.setPosition(tooltipCoord);
-            });
-        }, this);
+    draw.on('drawend', function (evt) {
+        //fid++;
+        //evt.feature.setId(fid);
+        measureTooltipElement.className = 'tooltip tooltip-static';
+        measureTooltip.setOffset([0, -7]);
+        // unset sketch
+        sketch = null;
+        createMeasureTooltip();
 
-    draw.on('drawend',
-        function () {
-            measureTooltipElement.className = 'tooltip tooltip-static';
-            measureTooltip.setOffset([0, -7]);
-            // unset sketch
-            sketch = null;
-            // unset tooltip so that a new one can be created
-            measureTooltipElement = null;
-            createMeasureTooltip();
-            ol.Observable.unByKey(listener);
-        }, this);
-
-    var modify = new ol.interaction.Modify({
-        features: features,
-        // the SHIFT key must be pressed to delete vertices, so
-        // that new vertices can be drawn at the same position
-        // of existing vertices
-        deleteCondition: function (event) {
-            return ol.events.condition.shiftKeyOnly(event) &&
-                ol.events.condition.singleClick(event);
-        }
-    });
-    map.addInteraction(modify);
+    }, this);
 
     createMeasureTooltip();
     createHelpTooltip();
 }
+var geom;
+var select = new ol.interaction.Select({
+    wrapX: false,
+    hitTolerance: 5
+});
 
+var measureInfoElement;
+var measureInfo;
+select.on('select', function (evt) {
+    if (evt.selected.length > 0) {
+        el_btn_feature_del.hidden = false;
+        map.removeOverlay(measureInfo);
+        var feat = evt.target.getFeatures().item(0).getGeometry();
+        var output;
+        var infoCoord;
 
+        if (feat instanceof ol.geom.Polygon) {
+            output = formatArea(feat);
+            infoCoord = feat.getInteriorPoint().getCoordinates();
+        } else if (feat instanceof ol.geom.LineString) {
+            output = formatLength(feat);
+            infoCoord = feat.getLastCoordinate();
+        }
+        measureInfoElement = document.createElement('div');
+        measureInfoElement.id = 'mInfoEl';
+        measureInfoElement.className = 'tooltip tooltip-static';
+        measureInfoElement.innerHTML = output;
+        measureInfo = new ol.Overlay({
+            element: measureInfoElement,
+            offset: [0, -15],
+            positioning: 'bottom-center'
+        });
+        map.addOverlay(measureInfo);
+        measureInfo.setPosition(infoCoord);
+
+        function delFeature() {
+            if (select.getFeatures().getLength() > 0) {
+                var f = select.getFeatures().item(0);
+                vector.getSource().removeFeature(f);
+                select.getFeatures().clear();
+            }
+            measureInfoElement.hidden = true;
+            el_btn_feature_del.hidden = true;
+
+            el_btn_feature_del.removeEventListener('click', delFeature, false);
+            el_btn_feature_del.removeEventListener('touchstart', delFeature, false);
+        }
+
+        el_btn_feature_del.addEventListener('click', delFeature, false);
+        el_btn_feature_del.addEventListener('touchstart', delFeature, false);
+
+    } else {
+        el_btn_feature_del.hidden = true;
+    }
+
+}, this);
+var t;
+var modify = new ol.interaction.Modify({
+    features: select.getFeatures(),
+    // the SHIFT key must be pressed to delete vertices, so
+    // that new vertices can be drawn at the same position
+    // of existing vertices
+    deleteCondition: function (event) {
+        return ol.events.condition.shiftKeyOnly(event) &&
+            ol.events.condition.singleClick(event);
+    }
+});
+//var test;
+modify.on('modifystart', function (ev) { map.removeOverlay(measureInfo); }, this);
+////modify.on('modifyend', function (ev) { measureDrawEnd(); }, this);
 
 /**
  * Creates a new help tooltip
@@ -413,20 +442,9 @@ function createMeasureTooltip() {
     measureTooltipElement.className = 'tooltip tooltip-measure';
     measureTooltip = new ol.Overlay({
         element: measureTooltipElement,
-        offset: [0, -15],
+        offset: [-50, -25],
         positioning: 'bottom-center'
     });
     map.addOverlay(measureTooltip);
 }
 
-
-
-/**
- * Let user change the geometry type.
- */
-//typeSelect.onchange = function () {
-//    map.removeInteraction(draw);
-//    addInteraction();
-//};
-
-addInteraction();
